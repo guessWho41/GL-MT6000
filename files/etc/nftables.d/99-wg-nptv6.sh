7faab5925437fc83ebd243e8bbda026342de798f
guessWho41 <251509162+guessWho41@users.noreply.github.com>
2026-04-17 03:12:02 +0200

    Update 99-wg-nptv6.sh

--- files/etc/nftables.d/99-wg-nptv6.sh
+++ files/etc/nftables.d/99-wg-nptv6.sh
@@ -11,7 +11,6 @@ network_get_prefix6 WAN_PFX "$WAN6_IF" || exit 0
 
 WG_INTERFACES=$(ubus call network.interface dump | jsonfilter -e '@.interface[@.proto="wireguard"].interface')
 [ -z "$WG_INTERFACES" ] && exit 0
-
 for WG_IF in $WG_INTERFACES; do
     WG_STATUS=$(ubus call network.interface."$WG_IF" status 2>/dev/null)
     [ "$(echo "$WG_STATUS" | jsonfilter -e '@.up')" != "true" ] && continue
