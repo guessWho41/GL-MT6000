0546612ff8772c8b7429aa1ad9666b4368adad8c
guessWho41 <251509162+guessWho41@users.noreply.github.com>
2026-04-17 03:05:52 +0200

    Update 99-wg-nptv6.sh

--- files/etc/nftables.d/99-wg-nptv6.sh
+++ files/etc/nftables.d/99-wg-nptv6.sh
@@ -2,25 +2,24 @@
 
 . /lib/functions/network.sh
 
-WG_INTERFACES=$(ubus call network.interface dump | jsonfilter -e '@.interface[@.proto="wireguard"].interface')
-[ -z "$WG_INTERFACES" ] && exit 0
-
 network_find_wan6 WAN6_IF
 [ -z "$WAN6_IF" ] && exit 0
 
 network_flush_cache
-if ! network_get_device WAN_DEV "$WAN6_IF" || ! network_get_prefix6 WAN_PFX "$WAN6_IF" || [ -z "$WAN_PFX" ]; then
-    exit 0
-fi
+network_get_device WAN_DEV "$WAN6_IF" || exit 0
+network_get_prefix6 WAN_PFX "$WAN6_IF" || exit 0
+
+WG_INTERFACES=$(ubus call network.interface dump | jsonfilter -e '@.interface[@.proto="wireguard"].interface')
+[ -z "$WG_INTERFACES" ] && exit 0
 
 for WG_IF in $WG_INTERFACES; do
     WG_STATUS=$(ubus call network.interface."$WG_IF" status 2>/dev/null)
     [ "$(echo "$WG_STATUS" | jsonfilter -e '@.up')" != "true" ] && continue
 
-    eval $(echo "$WG_STATUS" | jsonfilter -e WG_ADDR='@["ipv6-address"][0].address' -e WG_MASK='@["ipv6-address"][0].mask')
-    if [ -z "$WG_ADDR" ] || [ -z "$WG_MASK" ]; then
-        continue
-    fi
-    WG_PFX="${WG_ADDR%:*}:/${WG_MASK}"
-    nft add rule inet fw4 srcnat oifname "${WAN_DEV}" snat ip6 prefix to ip6 saddr map { "${WG_PFX}" : "${WAN_PFX}" }
+    WG_ADDR=$(echo "$WG_STATUS" | jsonfilter -e '@["ipv6-address"][0].address')
+    [ -n "$WG_ADDR" ] || continue
+    WG_MASK=$(echo "$WG_STATUS" | jsonfilter -e '@["ipv6-address"][0].mask')
+    [ -n "$WG_MASK" ] || continue
+
+    nft add rule inet fw4 srcnat oifname "${WAN_DEV}" snat ip6 prefix to ip6 saddr map { "${WG_ADDR}/${WG_MASK}" : "${WAN_PFX}" }
 done
